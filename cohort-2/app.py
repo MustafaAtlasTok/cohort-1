1d2ecbbf0ca63cdb9719c882694fa30fd7f73146
Mustafa Tok <mustafatok@costco.com>
2025-06-13 12:08:22 -0500

    update

--- cohort-2/app.py
+++ cohort-2/app.py
@@ -11,3 +11,8 @@ def hello():
 if __name__ == '__main__':
     # bind to 0.0.0.0 so Docker can route traffic in
     app.run(host='0.0.0.0', port=8080)
+
+# docker build -t myflaskapp:latest . 
+
+
+# docker run -d --name flask_container -p 8080:8080 -e APP_NAME="DockerLearner" myflasktask:latest
